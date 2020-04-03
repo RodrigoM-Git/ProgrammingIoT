c9cf48d50c48fd0848fe91760436c45425cb7a62
Rodrigo Miguel Rojas s3784466 <s3784466@student.rmit.edu.au>
2020-04-03 19:58:29 +1100

    Edited AnimatedEmoji.py

--- Assignment 1/AnimatedEmoji.py
+++ Assignment 1/AnimatedEmoji.py
@@ -2,4 +2,12 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
+b = (0, 0, 0) #black
+y = (0, 250, 250) #yellow
+
+
+class emoji(object):
+    def __init__(seld):
+        return sense_set_pixels(object)
+
 
