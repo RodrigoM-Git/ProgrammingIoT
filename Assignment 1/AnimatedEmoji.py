fcaff9b0d87b4935f41173934711788091003d3b
Rodrigo Miguel Rojas <s3784466@student.rmit.edu.au>
2020-04-03 22:01:15 +1100

    Finished AnimatedEmoji.py

--- Assignment 1/AnimatedEmoji.py
+++ Assignment 1/AnimatedEmoji.py
@@ -1,13 +1,74 @@
 from sense_hat import SenseHat
+from time import sleep
 
 sense = SenseHat()
 
+y = (250, 250, 0) #yellow
+bl = (0, 0, 250) #blue
+r = (250, 0, 0) #red
 b = (0, 0, 0) #black
-y = (0, 250, 250) #yellow
 
+class emoji():
+    
+    def getHappy(self, color):
+        c = color
+        
+        face = [
+            c, c, c, c, c, c, c, c,
+            c, c, c, c, c, c, c, c,
+            c, b, b, c, c, b, b, c,
+            c, b, b, c, c, b, b, c,
+            c, c, c, c, c, c, c, c,
+            c, b, b, c, c, b, b, c,
+            c, c, c, b, b, c, c, c,
+            c, c, c, c, c, c, c, c
+        ] 
+        return face
 
-class emoji(object):
-    def __init__(seld):
-        return sense_set_pixels(object)
-
-
+    def getSad(self, color):
+        c = color
+        
+        face = [
+            c, c, c, c, c, c, c, c,
+            c, c, c, c, c, c, c, c,
+            c, b, b, c, c, b, b, c,
+            c, b, b, c, c, b, b, c,
+            c, c, c, c, c, c, c, c,
+            c, c, c, b, b, c, c, c,
+            c, b, b, c, c, b, b, c,
+            c, c, c, c, c, c, c, c
+        ]
+        return face
+    
+    def getWink(self, color):
+        c = color
+        
+        face = [
+            c, c, c, c, c, c, c, c,
+            c, c, c, c, c, c, c, c,
+            c, b, b, c, c, c, c, c,
+            c, b, b, c, c, b, b, c,
+            c, c, c, c, c, c, c, c,
+            c, c, c, c, c, b, b, c,
+            c, b, b, b, b, c, c, c,
+            c, c, c, c, c, c, c, c
+        ] 
+        return face
+     
+    
+while True:
+    happy = emoji()
+    sad = emoji()
+    wink = emoji()
+    
+    sense.set_pixels(happy.getHappy(y))
+    sleep(3)
+    sense.set_pixels(sad.getSad(r))
+    sleep(3)
+    sense.set_pixels(wink.getWink(bl))
+    sleep(3)
+    
+    
+    
+    
+    
